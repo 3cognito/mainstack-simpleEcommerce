914242a0af16a62e5497ab7b568a4c4ff6577a3e
Tommy <tommyubongabasijohn@gmail.com>
2023-02-16 00:13:26 +0100

    initialise routes

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import productRouter from "./src/routes/product.route";
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ app.use(express.json());
 //Connect to database
 const connectToDb = async () => {
   const uri =
-    "mongodb+srv://Tommy:JTARPweG7AYsdVjQ@cluster0.gidll0n.mongodb.net/test"; //Move to env file
+    "mongodb+srv://Tommy:JTARPweG7AYsdVjQ@cluster0.gidll0n.mongodb.net/test"; //Fix into env file
   try {
     await mongoose.connect(uri);
     console.log("Connected to Database");
@@ -19,6 +20,7 @@ const connectToDb = async () => {
     process.exit(1);
   }
 };
+app.use(productRouter);
 
 //Start server
 const startExpressServer = async () => {
