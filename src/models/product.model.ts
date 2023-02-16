d849a98d52477724f080abdce067b5d24d0219b2
Tommy <tommyubongabasijohn@gmail.com>
2023-02-16 09:48:41 +0100

    minimal model

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -3,21 +3,13 @@ import mongoose, { Document, Schema } from "mongoose";
 export interface IProduct extends Document {
   name: string;
   price: number;
-  image: string;
   description: string;
-  available: number;
-  createdAt: Date;
-  updatedAt: Date;
 }
 
 const productSchema: Schema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  image: { type: String, required: true },
   description: { type: String, required: true },
-  available: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model<IProduct>("Product", productSchema);
