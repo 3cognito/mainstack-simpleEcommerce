2e1a7de28f29c48c537e271f6354cdae6dc5f6aa
Tommy <tommyubongabasijohn@gmail.com>
2023-02-16 11:36:04 +0100

    next fxn and error catches

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { IProduct } from "../models/product.model";
 import { ProductServices } from "../services/product.service";
 
@@ -9,48 +9,94 @@ export class ProductController {
     this.productService = new ProductServices();
   }
 
-  getAllProducts = async (req: Request, res: Response): Promise<void> => {
-    const products = await this.productService.getProducts(3, 4); //Temporary placeholder
-    res.send(products);
-  };
-
-  getProduct = async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    const product = await this.productService.getProduct(id);
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send({ message: "Product not found" });
+  getAllProducts = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      const { page = 1, limit = 10 } = req.query;
+      const pageNumber = +page ?? 1;
+      const limitNumber = +limit ?? 10;
+      const products = await this.productService.getProducts(
+        pageNumber,
+        limitNumber
+      );
+      res.send(products);
+    } catch (err) {
+      next(err);
     }
   };
 
-  createProduct = async (req: Request, res: Response): Promise<void> => {
-    const productData: IProduct = req.body;
-    const newProduct = await this.productService.createProduct(productData);
-    res.send(newProduct);
-  };
-
-  updateProduct = async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    const productData: IProduct = req.body;
-    const updatedProduct = await this.productService.updateProduct(
-      id,
-      productData
-    );
-    if (updatedProduct) {
-      res.send(updatedProduct);
-    } else {
-      res.status(404).send({ message: "Product not found" });
+  getProduct = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const product = await this.productService.getProduct(id);
+      if (product) {
+        res.send(product);
+      } else {
+        res.status(404).send({ message: "Product not found" });
+      }
+    } catch (err) {
+      next(err);
     }
   };
 
-  deleteProduct = async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    const deletedProduct = await this.productService.deleteProduct(id);
-    if (deletedProduct) {
-      res.send({ message: "Product deleted" });
-    } else {
-      res.status(404).send({ message: "Product not found" });
+  createProduct = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      const productData: IProduct = req.body;
+      const newProduct = await this.productService.createProduct(productData);
+      res.send(newProduct);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  updateProduct = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const productData: IProduct = req.body;
+      const updatedProduct = await this.productService.updateProduct(
+        id,
+        productData
+      );
+      if (updatedProduct) {
+        res.send(updatedProduct);
+      } else {
+        res.status(404).send({ message: "Product not found" });
+      }
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  deleteProduct = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const deletedProduct = await this.productService.deleteProduct(id);
+      if (deletedProduct) {
+        res.send({ message: "Product deleted" });
+      } else {
+        res.status(404).send({ message: "Product not found" });
+      }
+    } catch (err) {
+      next(err);
     }
   };
 }
