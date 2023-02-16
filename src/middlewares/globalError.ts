21855cda6f9ae11849ea432221767c26e590a5e2
Tommy <tommyubongabasijohn@gmail.com>
2023-02-16 11:35:36 +0100

    complete error handling

--- src/middlewares/globalError.ts
+++ src/middlewares/globalError.ts
@@ -1,8 +1,36 @@
 import { Request, Response, NextFunction } from "express";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+export async function notFound(req: Request, res: Response) {
+  res.status(404).send({ message: "Not found, Route does not exist" });
+}
 
 export async function errorHandler(
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  if (process.env.NODE_ENV === "developmen") {
+    console.error(err);
+  } else {
+    //Handle mongoose validation and inavlid ids
+    if (err instanceof mongoose.Error.ValidationError) {
+      const errors: any = {};
+      Object.keys(err.errors).forEach((key) => {
+        errors[key] = err.errors[key].message;
+      });
+      res.status(422).json({ errors });
+    } else if (
+      err instanceof mongoose.Error.CastError &&
+      err.kind === "ObjectId"
+    ) {
+      res.status(422).json({ message: "Invalid ID" });
+    } else {
+      console.error(err);
+      res.status(500).send({ message: "Internal Server error" });
+    }
+  }
+}
